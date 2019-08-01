e2d0f2a594f6c794b90083ea2deec76c263f8470
Alisa Stolbunova <alisa.stolbunova@gmail.com>
2019-08-01 13:35:22 +0300

    Task 1,2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,75 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world Alisa");
+        compareString();
+        choiceCase();
     }
+
+
+
+/*1. Запросить у пользователя 2 слова. Если они одинаковывы вести "Отлично! Слова одинаковы".Если они отличаются лишь регистром
+-"Хорошо. Почти одинаковы" Если они отличаются, но одной длины -"Ну, хотя бы они одной длины" ИспользоватьметодыклассаString:
+equals(String,string), equalsIgnoreCase(String string), length().*/
+
+    public static void compareString(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите первую строку: ");
+        String str1 = sc.next();
+        System.out.println("Введите вторую строку: ");
+        String str2 = sc.next();
+        sc.close();
+
+        if(str1.equals(str2)){
+            System.out.println("Отлично! Слова одинаковы");
+        }
+        else if(str1.equalsIgnoreCase(str2)){
+            System.out.println("Хорошо. Почти одинаковы");
+        }
+        else if(str1.length()==str2.length()){
+            System.out.println("Ну, хотя бы они одной длины");
+        }
+    }
+
+
+    /*2. Имеется целое число, вводимое пользователем с консоли. Это число - сумма денег в рублях.
+    Вывести это число, добавив к нему слово "рубль" в правильном падеже ("рубль", "рублей", "рубля").
+    a. Использовать конструкцию if-else.
+    b. Модифицировать в switch в отдельном файле*/
+
+    public static void choiceCase(){
+        String str;
+        int number;
+        boolean flag = true;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите целое число: ");
+        while(flag) {
+            str = sc.next();
+            if(str.equals("exit")){
+                flag = false;
+                break;
+            }
+            number = Integer.parseInt(str);
+
+            if (((number % 100) / 10) == 1) {
+                System.out.println(number + " рублей");
+            } else if ((number % 10 == 1)) {
+                System.out.println(number + " рубль");
+            } else if ((number % 10) > 1 && (number % 10) < 5) {
+                System.out.println(number + " рубля");
+            } else {
+                System.out.println(number + " рублей");
+            }
+        }
+
+        sc.close();
+    }
+
+
+
 }
+
+
+

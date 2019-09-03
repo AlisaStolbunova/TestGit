c8fef41fb498220467804c189e809f57b5ebdb3c
Alisa Stolbunova <alisa.stolbunova@gmail.com>
2019-09-03 11:54:36 +0300

    task 1

--- src/work/home/lesson9_regexp/Regexpr.java
+++ src/work/home/lesson9_regexp/Regexpr.java
@@ -5,6 +5,10 @@ import java.util.regex.Pattern;
 
 public class Regexpr {
 
+   /* Написать программу, проверяющую, является ли введённая строка адресом электронного почтового ящика.
+    В названии почтового ящика разрешить использование только букв, цифр и нижних подчёркиваний, при этом оно
+    должно начинаться с буквы. Возможные домены верхнего уровня: .org .com*/
+
     public static  boolean validMail(String str){
         String[] array = str.split("\\.");
         if("org".equals(array[1]) || "com".equals(array[1])){

402ab0c90a7af14343a19c04bee6f606108aaa37
Alisa Stolbunova <alisa.stolbunova@gmail.com>
2019-09-03 15:49:53 +0300

    task 1

--- src/work/home/lesson9_regexp/Regexpr.java
+++ src/work/home/lesson9_regexp/Regexpr.java
@@ -10,14 +10,9 @@ public class Regexpr {
     должно начинаться с буквы. Возможные домены верхнего уровня: .org .com*/
 
     public static  boolean validMail(String str){
-        String[] array = str.split("\\.");
-        if("org".equals(array[1]) || "com".equals(array[1])){
-            Pattern pattern = Pattern.compile("^([(A-Za-z])\\w*_*@[a-z]+");
-            Matcher matcher = pattern.matcher(array[0]);
-            return matcher.matches();
-        }
-        else
-            return false;
+       Pattern pattern = Pattern.compile("^[A-Za-z]\\w*_*@[a-z]*\\.(org|com)$");
+       Matcher matcher = pattern.matcher(str);
+       return matcher.matches();
     }
 
 
@@ -26,7 +21,7 @@ public class Regexpr {
 
 
     public static void main(String[] args) {
-        String str = "Alisa_st@gmail.com";
+        String str = "Alisa_st@gmail.org";
         System.out.println(validMail(str));
     }
 
